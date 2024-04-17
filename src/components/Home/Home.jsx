977131ccd4293146d665b40d6eb722a2ff6bf686
Sathish Jukanti <sathishjukanti@gmail.com>
2024-04-18 03:30:24 +0530

    Mobile optimization

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,17 +13,17 @@ function Home() {
   return (
     <>
       <div 
-      className='w-full md:min-h-screen h-[600px]'>
+      className='w-full md:min-h-screen h-[700px]'>
         <div 
         className='bg-slate-700'>
           <div 
-          className='md:min-h-screen w-full h-[600px] flex justify-center items-center'>
+          className='md:min-h-screen w-full h-[700px] flex justify-center items-center'>
             <div 
             className='md:min-h-[300px] md:min-w-[800px] min-h-[500px] w-[250px]'>
               <div 
               className='grid md:grid-cols-2 flex-row-reverse md:backdrop-blur-sm '>
                 <div 
-                className='md:w-[400px] w-[250px] min-h-full overflow-hidden'>
+                className='md:w-[400px] w-[250px] min-h-full overflow-hidden mt-16'>
                   <img 
                   className=' rounded-xl shadow-xl-white hover:scale-110 hover:duration-500 duration-700 '
                   src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
